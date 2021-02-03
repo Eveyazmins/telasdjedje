8a68ddde5a5dfad5de006f011b46fc51d24e4fbc
Evelin Santamaria <eveyazmins@gmail.com>
2021-02-02 23:30:35 -0300

    c

---
+++ client/build/precache-manifest.57bc3ee4c9d569ed21b6964879db0e41.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb9b8116d129ea9c0fa51d49856b5db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "39d151049ba1c17cc4eb",
+    "url": "/static/css/2.63343480.chunk.css"
+  },
+  {
+    "revision": "6af381bc9c7e1118e371",
+    "url": "/static/css/main.f17218a5.chunk.css"
+  },
+  {
+    "revision": "39d151049ba1c17cc4eb",
+    "url": "/static/js/2.38283ce8.chunk.js"
+  },
+  {
+    "revision": "98c92e4a71422a59c3daed74f16a0206",
+    "url": "/static/js/2.38283ce8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6af381bc9c7e1118e371",
+    "url": "/static/js/main.808801b2.chunk.js"
+  },
+  {
+    "revision": "0cb516cdc09d15f00029",
+    "url": "/static/js/runtime-main.d74c7a97.js"
+  },
+  {
+    "revision": "743363c821ffeb8f01505fdf3c5b924a",
+    "url": "/static/media/rollos.743363c8.jpg"
+  }
+]);
